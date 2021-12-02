f928058599bce158a80ed4cf0bd5e1217ce489fe
Elad Hakmon <eladhakmon11@gmail.com>
2021-12-02 19:36:40 +0200

    updates to deploy to heroku

--- src/app/server.js
+++ src/app/server.js
@@ -1,20 +1,8 @@
-//Install express server
 const express = require('express');
 const path = require('path');
-
 const app = express();
-
-// Serve only the static files form the dist directory
-app.use(express.static('./dist/gym-project'));
-
-app.get('/*', (req, res) =>
-    res.sendFile('index.html', {root: 'dist/gym-project/'}),
-);
-
-// Start the app by listening on the default Heroku port
+app.use(express.static(__dirname + '/dist/gym-project'));
+app.get('/*', function(req,res) {
+res.sendFile(path.join(__dirname+
+'/dist/gym-project/index.html'));});
 app.listen(process.env.PORT || 8080);
-
- 
- 
-
- 
